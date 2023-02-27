4eb70a5c46f64db7e162f59ad550233e7d14f337
munyamugb <munyaradzi045@gmail.com>
2023-02-27 21:00:32 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles/main.scss';
+
+alert('Hello if this worked you are not an idiot');
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Belden to do list app',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+}
